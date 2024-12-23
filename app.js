15f01441037da2bdf67295d9ed054cd92376d714
Danish Ali Umar <danishaliumar4@gmail.com>
2024-12-23 07:03:05 +0500

    Create simple animated circle  by uisng sine and cosine

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
 const material = new THREE.MeshNormalMaterial();
 
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+// const mesh = new THREE.Mesh(geometry, material);
+// scene.add(mesh);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(width, height);
@@ -31,9 +31,46 @@ const divisions = 40;
 const gridHelper = new THREE.GridHelper(size, divisions);
 scene.add(gridHelper);
 
+let mesh = new THREE.Mesh(new THREE.SphereGeometry(0.02, 32, 32), new THREE.MeshNormalMaterial());
+
+function addPoint(x, y, z) {
+    let point = mesh.clone()
+    point.position.set(x, y, z)
+    scene.add(point)
+    return point
+}
+
+let number = 300
+let objects = []
+for (let i = 0; i < number; i++) {
+    let theta = i / number * Math.PI * 2;
+    let x = Math.cos(theta)
+    let y = Math.sin(theta)
+    let z = 0
+    let mesh = addPoint(x, y, z)
+
+    objects.push({
+        mesh,
+        theta,
+        random: Math.random(),
+        x: Math.random() / 5,
+        y: Math.random() / 5,
+        z: Math.random() / 5,
+    })
+}
+
 // animation
 
 function animate(time) {
+
+    objects.forEach((object, i) => {
+        let { mesh, theta, random, x, y, z } = object
+        let t = time / 1000
+        let newX = Math.cos(theta + t + x)
+        let newY = Math.sin(theta + t + y)
+        let newZ = z
+        mesh.position.set(newX, newY, newZ)
+    })
 
     mesh.rotation.x = time / 2000;
     mesh.rotation.y = time / 1000;
